c5dd84a1a860554cd24ececfdfc804251d336816
nouf <noufrashid594@gmail.com>
2025-10-20 12:22:17 +0300

    Initial Commit

---
+++ plant/plantApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantApp.swift
+//  plant
+//
+//  Created by nouf on 28/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
